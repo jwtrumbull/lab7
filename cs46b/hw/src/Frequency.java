ee4062309dccd784496146476f2e1028def884d5
Jordan <jwtrumbull@yahoo.com>
2015-10-21 14:20:46 -0700

    first commit

--- cs46b/hw/src/Frequency.java
+++ cs46b/hw/src/Frequency.java
@@ -8,6 +8,18 @@ public class Frequency
 	 */
 	public static int[] frequencyOfElements(int[] a)
 	{
+		/*
+		 * array starts at i = 0 and j = 1
+		 * check if the array in pos i and j are the same
+		 * if so add to the counter
+		 * then have j go to the next pos
+		 * repeat this until you get the the last elt in the array
+		 * then add the counter to the other array that keeps count at pos i
+		 * then increment i by 1 and go through j again
+		 * go through the length of array b
+		 * then return array b
+		 * 
+		 */
 		int[] b = new int[a.length]; 
 		for (int i = 0; i < b.length; i++)
 		{
